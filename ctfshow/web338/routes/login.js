4da6a65e5e7f4a5f81a37390da44a8de806225f9
bolerat <bolerat@outlook.com>
2024-12-21 00:31:55 +0800

    current work

---
+++ ctfshow/web338/routes/login.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var utils = require('../utils/common');
+
+
+
+/* GET home page.  */
+router.post('/', require('body-parser').json(),function(req, res, next) {
+  res.type('html');
+  var flag='flag_here';
+  var secert = {};
+  var sess = req.session;
+  let user = {};
+  utils.copy(user,req.body);
+  if(secert.ctfshow==='36dboy'){
+    res.end(flag);
+  }else{
+    return res.json({ret_code: 2, ret_msg: '登录失败'+JSON.stringify(user)});  
+  }
+  
+  
+});
+
+module.exports = router;
